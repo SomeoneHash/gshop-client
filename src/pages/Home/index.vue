e9b828878943c9d4b1074279689f331c716ab07f
liujiwen <1010068051@qq.com>
2021-08-02 23:30:23 +0800

    'Home'

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="home">Home</div>
+  <div>
+    <TypeNav/>
+    <ListContainer/>
+    <TodyRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Brand/>
+  </div>
 </template>
 
 <script>
+import TypeNav from '@/components/TypeNav'
+import ListContainer from './ListContainer'
+import TodyRecommend from './TodyRecommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
   name: 'Home',
+  components:{
+    TypeNav,
+    ListContainer,
+    TodyRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  }
 }
 </script>
 
-<style lang="less" scoped>
-  .home{
-    color: #bfa;
-  }
-</style>
+
